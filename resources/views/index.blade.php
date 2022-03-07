c24a4556a67b390fdb9738741933fe18b4abc255
Kenneth Kipchumba <branrono@gmail.com>
2022-03-07 14:55:37 +0300

    Login Pop Up after a while of browsing on the landing page. Laravel fortify installed

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,21 +1,18 @@
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
   <title>TIFA Opinion Polls</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="{{ asset('assets/custom/auth_popup.css') }}" type="text/css">
+
   <link href="{{ asset('assets/twbs/styles/bootstrap.css') }}" rel="stylesheet">
   <script src="{{ asset('assets/twbs/js/bootstrap.bundle.js') }}"></script>
-  <style>
-  .fakeimg {
-    height: 200px;
-    background: #aaa;
-  }
-  </style>
 </head>
 <body>
 
-<nav class="navbar navbar-expand-sm navbar-dark bg-primary">
+  <nav class="navbar navbar-expand-sm navbar-dark bg-primary">
   <div class="container-fluid">
     <a class="navbar-brand" href="javascript:void(0)">
         <img src="{{ asset('images/logo.png') }}" alt="TIFA Opinion Polls">
@@ -38,7 +35,44 @@
   </div>
 </nav>
 
-<div class="container-fluid bg-dark">
+  <div class="login-popup">
+     <div class="box">
+       <div class="img-area">
+        <div class="img">
+        </div>
+       </div>
+       <div class="form">
+        <div class="close">&times;</div>
+        <h1>Log In</h1>
+        @if ($errors->any())
+          <div class="alert alert-danger">
+            <ul>
+              @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+              @endforeach
+            </ul>
+          </div>
+        @endif
+        <form action="{{ url('login') }}" method="POST">
+          @csrf
+          <div class="form-group">
+            <input type="text" name="email" placeholder="Email" class="form-control">
+          </div>
+          <div class="form-group">
+            <input type="password" name="password" placeholder="Password" class="form-control">
+          </div>
+          <div class="form-group">
+            <label><input type="checkbox">
+              Remember Me
+            </label>
+          </div>
+          <button type="submit" class="btn">Log In</button>
+        </form>
+       </div>
+     </div>
+  </div>
+
+  <div class="container-fluid bg-dark">
   <!-- Main content here -->
      <iframe width="100%" height="950" src="https://datastudio.google.com/embed/reporting/cc23d1f8-bc58-4d2f-853c-e7660cf05ffe/page/d3mlC" frameborder="0" style="border:0" allowfullscreen></iframe> 
 </div>
@@ -47,5 +81,6 @@
   <p>Footer</p>
 </div>
 
+<script src="{{ asset('assets/custom/auth_popup.js') }}"></script>
 </body>
 </html>
