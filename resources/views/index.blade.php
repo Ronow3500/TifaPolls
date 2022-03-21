1de7c038a7508d351f20fab246ce48f012af56ae
Kenneth Kipchumba <branrono@gmail.com>
2022-03-21 10:42:00 +0300

    Updated the Iframe link to point to updated data studio

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -10,7 +10,7 @@
     </div>
   @endif
   
-  <iframe width="100%" height="950" src="https://datastudio.google.com/embed/reporting/cc23d1f8-bc58-4d2f-853c-e7660cf05ffe/page/d3mlC" frameborder="0" style="border:0" allowfullscreen></iframe>
+  <iframe width="100%" height="950" src="https://datastudio.google.com/embed/reporting/40a80583-7517-4373-85f2-fdf52322dcc2/page/Z9VoC" frameborder="0" style="border:0" allowfullscreen></iframe>
 
 
   @guest   
