e81629c99ec669c62ac57995a9907aef28252225
Kenneth Kipchumba <branrono@gmail.com>
2022-05-11 14:00:50 +0300

    Added primary alert for status

--- resources/views/layouts/alerts.blade.php
+++ resources/views/layouts/alerts.blade.php
@@ -10,6 +10,12 @@
   </div>
 @endif
 
+@if(session('status'))
+  <div class="alert alert-primary" role="alert">
+    {{ session('status') }}
+  </div>
+@endif
+
 @if(session('warning'))
   <div class="alert alert-warning" role="alert">
   	{{ session('warning') }}
@@ -19,5 +25,15 @@
 @if(session('error'))
   <div class="alert alert-danger" role="alert">
   	{{ session('error') }}
+  </div>
+@endif
+
+@if ($errors->any())
+  <div class="alert alert-danger">
+    <ul>
+      @foreach ($errors->all() as $error)
+        <li>{{ $error }}</li>
+      @endforeach
+    </ul>
   </div>
 @endif
